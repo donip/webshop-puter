a15b9d0e241fa18ad93ef52ddc71868b22353a97
Peter Szarka <szarka98@gmail.com>
2018-05-14 15:33:47 +0200

    login and register

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,11 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HttpModule } from '@angular/http';
 import {FormsModule} from '@angular/forms';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/login/login.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { Http, RequestOptions } from '@angular/http';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  user: any = {
+    username: 'YOUR REGISTERED EMAIL',
+    password: 'YOUR REGISTERED USER PASSWORD',
+  };
+
+  options = new RequestOptions({ withCredentials: true });
+  baseUrl = 'http://localhost:8080/user/';
+
+
+  constructor(public http: Http) { }
+
+  profile() {
+    this.http.get(this.baseUrl + 'profile', this.options)
+        .subscribe(data => {
+            console.log(data['_body']);
+        });
+  }
+
+  login() {
+    this.http.post(this.baseUrl + 'login', this.user, this.options)
+        .subscribe(data => {
+            console.log(data['_body']);
+        });
+  }
+
+  logout() {
+    this.http.get(this.baseUrl + 'logout', this.options)
+        .subscribe(data => {
+            console.log(data['_body']);
+        });
+  }
+
+  ngOnInit() {
+  }
+
+}
