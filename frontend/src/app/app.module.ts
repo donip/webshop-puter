9f970367fb414fea9d782578c35f40dbbf0db855
Doni Pataki <yellow.pataki.doni@gmail.com>
2018-05-14 10:52:12 +0200

    feat: main pages at frontend

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,11 +5,19 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HttpModule } from '@angular/http';
 import {FormsModule} from '@angular/forms';
+import { StatisticsComponent } from './statistics/statistics.component';
+import { ProductsComponent } from './products/products.component';
+import { UsersComponent } from './users/users.component';
+import { OrdersComponent } from './orders/orders.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StatisticsComponent,
+    ProductsComponent,
+    UsersComponent,
+    OrdersComponent
   ],
   imports: [
     BrowserModule,
