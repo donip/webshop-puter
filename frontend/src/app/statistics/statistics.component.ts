006165d100d6ee06e204b7b9ff865597a8fbdcd1
Szücs Máté <none@ofyourbiz.com>
2018-05-15 13:55:40 +0200

    műkszik?

--- frontend/src/app/statistics/statistics.component.ts
+++ frontend/src/app/statistics/statistics.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit, NgModule} from '@angular/core';
+import { Component, OnInit, NgModule } from '@angular/core';
+import { Http, RequestOptions } from '@angular/http';
+import { ProductsComponent } from '../products/products.component';
+import { Observable } from 'rxjs/Observable';
 
 
 @Component({
@@ -8,7 +11,7 @@ import { Component, OnInit, NgModule} from '@angular/core';
 })
 @NgModule({
   imports: [
-   ,
+    ,
   ],
 })
 
@@ -16,42 +19,59 @@ export class StatisticsComponent implements OnInit {
   a = Math.floor(Math.random() * Math.floor(200));
   b = Math.floor(Math.random() * Math.floor(200));
   c = Math.floor(Math.random() * Math.floor(200));
-  d = Math.floor(Math.random() * Math.floor(this.a));
-  pieChartData =  {
+  d = Math.floor(Math.random() * Math.floor(200));
+  productcat: any;
+  options = new RequestOptions({ withCredentials: true });
+  income: number;
+  pieChartData = {
     chartType: 'ColumnChart',
     dataTable: [
       ['0', '0'],
-      ['Felhasználók',     this.a],
-      ['Eladott termék',      this.b],
-      ['Termék ára',  this.d],
+      ['Felhasználók', this.a],
+      ['Eladott termék', this.b],
+      ['Termék ára', this.d],
       ['Nem vásárló felhasználók', (this.b - this.a) * (-1)],
-      ['SEgy főre eső átlagos bevétel',    (this.c * this.d) / this.b]
+      ['SEgy főre eső átlagos bevétel', (this.c * this.d) / this.b]
     ],
-    options: {'title': 'Statisztika',
-    legend: 'none'},
+    options: {
+      'title': 'Statisztika',
+      legend: 'none'
+    },
   };
-  pieChartData2 =  {
+  pieChartData2 = {
     chartType: 'ColumnChart',
     dataTable: [
       ['0', '0'],
-      ['Felhasználók',     this.a],
-      ['Eladott termék',      this.b],
-      ['Termék ára',  this.d],
+      ['Felhasználók', this.a],
+      ['Eladott termék', this.b],
+      ['Termék ára', this.d],
       ['Nem vásárló felhasználók', (this.b - this.a) * (-1)],
-      ['SEgy főre eső átlagos bevétel',    (this.c * this.d) / this.b * 2]
+      ['SEgy főre eső átlagos bevétel', (this.c * this.d) / this.b * 2]
     ],
-    options: {'title': 'Statisztika',
-    legend: 'none'},
+    options: {
+      'title': 'Statisztika',
+      legend: 'none'
+    },
   };
-
-  constructor() {
-
-   }
-
-  ngOnInit() {
-
-    }
+  constructor(public http: Http) {
+    this.getProducts();
   }
 
-
-
+  ngOnInit() {
+  }
+  getProducts() {
+    this.http.get('http://localhost:8080/product', this.options)
+      .subscribe(getProducts => {
+        this.productcat = JSON.parse(getProducts['_body']);
+        console.log(this.productcat);
+        this.Income();
+      });
+  }
+  Income() {
+    this.income = 0;
+    for (let i = 0; i < (this.productcat).length; i++) {
+      this.income += ((this.productcat)[i]).price;
+    }
+    console.log(this.income);
+  }
+}
