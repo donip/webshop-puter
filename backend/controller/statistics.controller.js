eb56e8e1367d3b90207596e418c7a48eb680176d
Szücs Máté <none@ofyourbiz.com>
2018-05-15 14:05:15 +0200

    working

--- backend/controller/statistics.controller.js
+++ backend/controller/statistics.controller.js
@@ -1,1 +1,8 @@
-import {Product} from (/controller/Product)
+const Product = require('../models/product');
+const Order = require('../controller/order')
+module.exports = {
+    getProducts: (req, res) => {
+        Product.find({}).then(product => res.json(product))
+            .catch(err => res.send(err));
+    }
+}
