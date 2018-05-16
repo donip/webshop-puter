5372e58be9602c31b81d92f18fd4205d56ea6f19
Szücs Máté <none@ofyourbiz.com>
2018-05-16 15:02:11 +0200

    Statisztika működik

--- frontend/src/app/statistics/statistics.component.ts
+++ frontend/src/app/statistics/statistics.component.ts
@@ -1,8 +1,8 @@
+
 import { Component, OnInit, NgModule } from '@angular/core';
 import { Http, RequestOptions } from '@angular/http';
 import { ProductsComponent } from '../products/products.component';
-import { Observable } from 'rxjs/Observable';
-
+import { Ng2GoogleChartsModule } from 'ng2-google-charts';
 
 @Component({
   selector: 'app-statistics',
@@ -16,37 +16,26 @@ import { Observable } from 'rxjs/Observable';
 })
 
 export class StatisticsComponent implements OnInit {
-  a = Math.floor(Math.random() * Math.floor(200));
   b = Math.floor(Math.random() * Math.floor(200));
   c = Math.floor(Math.random() * Math.floor(200));
   d = Math.floor(Math.random() * Math.floor(200));
   productcat: any;
+  allusers: any;
+  allorders: any;
   options = new RequestOptions({ withCredentials: true });
   income: number;
+  sumuser: number;
+  sumbuyer: number;
+  sumsoldstuff: number;
   pieChartData = {
     chartType: 'ColumnChart',
     dataTable: [
       ['0', '0'],
-      ['Felhasználók', this.a],
+      ['Felhasználók', this.allusers],
       ['Eladott termék', this.b],
       ['Termék ára', this.d],
-      ['Nem vásárló felhasználók', (this.b - this.a) * (-1)],
+      ['Nem vásárló felhasználók', (this.b - this.allusers) * (-1)],
       ['SEgy főre eső átlagos bevétel', (this.c * this.d) / this.b]
-    ],
-    options: {
-      'title': 'Statisztika',
-      legend: 'none'
-    },
-  };
-  pieChartData2 = {
-    chartType: 'ColumnChart',
-    dataTable: [
-      ['0', '0'],
-      ['Felhasználók', this.a],
-      ['Eladott termék', this.b],
-      ['Termék ára', this.d],
-      ['Nem vásárló felhasználók', (this.b - this.a) * (-1)],
-      ['SEgy főre eső átlagos bevétel', (this.c * this.d) / this.b * 2]
     ],
     options: {
       'title': 'Statisztika',
@@ -55,7 +44,9 @@ export class StatisticsComponent implements OnInit {
   };
 
   constructor(public http: Http) {
+    this.getUsers();
     this.getProducts();
+    this.getOrders();
   }
 
   ngOnInit() {
@@ -64,15 +55,41 @@ export class StatisticsComponent implements OnInit {
     this.http.get('http://localhost:8080/product', this.options)
       .subscribe(getProducts => {
         this.productcat = JSON.parse(getProducts['_body']);
-        console.log(this.productcat);
-        this.Income();
       });
   }
-  Income() {
-    this.income = 0;
-    for (let i = 0; i < (this.productcat).length; i++) {
-      this.income += ((this.productcat)[i]).price;
-    }
-    console.log(this.income);
+  getUsers() {
+    this.http.get('http://localhost:8080/useradmin', this.options)
+      .subscribe(getUsers => {
+        this.allusers = JSON.parse(getUsers['_body']);
+        this.sumuser = this.Lengthening(this.allusers);
+      });
   }
+
+  getOrders() {
+    this.http.get('http://localhost:8080/order', this.options)
+  .subscribe(getOrders => {
+    this.allorders = JSON.parse(getOrders['_body']);
+  this.Income(this.allorders);
+  });
+}
+
+  Income(adat) {
+    this.income = 0;
+    this.sumsoldstuff = 0;
+    let sumprice = 0;
+    let sumsold = 0;
+    this.sumbuyer = this.Lengthening(adat);
+    for (let i = 0; i < adat.length; i++) {
+      for (let j = 0; j < adat[i].products.length; j++) {
+        sumprice += adat[i].products[j].product.price * adat[i].products[j].quantity;
+        sumsold += adat[i].products[j].quantity;
+      }
+      this.sumsoldstuff += sumsold;
+      this.income += sumprice;
+    }
+  }
+  Lengthening(array) {
+    return array.length;
+  }
+
 }
