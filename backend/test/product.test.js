8d85c0376f2082ea7114ed41db03610dbf0770c4
Peter Szarka <szarka98@gmail.com>
2018-05-16 19:44:27 +0200

    product controller test

--- backend/test/product.test.js
+++ backend/test/product.test.js
@@ -3,6 +3,7 @@ const {
   expect,
 } = require('chai');
 const chaiHttp = require('chai-http');
+
 const baseUrl = 'http://localhost:8080/product';
 chai.use(chaiHttp);
 const theAccount = {
@@ -50,50 +51,40 @@ describe('product.controller functions', () => {
         });
     });
   });
-/*
   describe('create()', () => {
-    // Login során kapunk egy sütit a http headerbe, ezt lementjük a süti változóba
-    // Ez azért kell mert minden kérésnél, amihez szükséges a belépett user,
-    // el kell küldeni a kapott sütit is. Hiszen ez azonosítja a usert
-    // Itt nincs böngésző ami lementse, így manuálisan kell
-    before((done) => {
-      chai.request('http://localhost:8080/user')
-        .post('/login')
-        .send(theAccount)
-        .end((err, res) => {
-          if (err) {
-            throw err;
-          }
-          cookie = res.headers['set-cookie'].pop().split(';')[0];
-          done();
-        });
-    });
-    
-    
     it('response statusCode equal to 200', (done) => {
       chai.request(baseUrl)
-        .put('/5afab001e8a028273ccecb24')
-        // A sütit visszaküldjük minden kérésnél, ahol kell a user azonosítása
-        .set('Cookie', cookie)
-        .end((err, res) => {
-          expect(res).to.have.status(200);
-          console.log(cookie);
-          done();
-        });
-    });
-  });
-
-  describe('removeUser()', () => {
-    it('response statusCode equal to 200', (done) => {
-      chai.request(baseUrl)
-        .delete('/5afb311edfdd372d041dda96')
-        // A sütit visszaküldjük minden kérésnél, ahol kell a user azonosítása
-        .set('Cookie', cookie)
+        .post('/')
+        .send({
+          productname: 'Bogi', brand: 'bogi', price: 'Bogi', category: 'kat',
+        })
         .end((err, res) => {
           expect(res).to.have.status(200);
           done();
         });
     });
   });
-  */
+  describe('update()', () => {
+    it('response statusCode equal to 200', (done) => {
+      chai.request(baseUrl)
+        .put('/termeek')
+        .send({
+          productname: 'Bogi', brand: 'bogi', price: 'Bogi', category: 'kat',
+        })
+        .end((err, res) => {
+          expect(res).to.have.status(200);
+          done();
+        });
+    });
+  });
+  describe('remove()', () => {
+    it('response statusCode equal to 200', (done) => {
+      chai.request(baseUrl)
+        .delete('/termeek')
+        .end((err, res) => {
+          expect(res).to.have.status(200);
+          done();
+        });
+    });
+  });
 });
--- backend/test/useradmin.test.js
+++ backend/test/useradmin.test.js
@@ -3,6 +3,7 @@ const {
   expect,
 } = require('chai');
 const chaiHttp = require('chai-http');
+
 const baseUrl = 'http://localhost:8080/useradmin';
 chai.use(chaiHttp);
 const theAccount = {
@@ -29,24 +30,24 @@ describe('useradmin.controller functions', () => {
     });
   });
 
-  describe('editUser()', () => {
-    // Login során kapunk egy sütit a http headerbe, ezt lementjük a süti változóba
-    // Ez azért kell mert minden kérésnél, amihez szükséges a belépett user,
-    // el kell küldeni a kapott sütit is. Hiszen ez azonosítja a usert
-    // Itt nincs böngésző ami lementse, így manuálisan kell
-    before((done) => {
-      chai.request('http://localhost:8080/user')
-        .post('/login')
-        .send(theAccount)
-        .end((err, res) => {
-          if (err) {
-            throw err;
-          }
-          cookie = res.headers['set-cookie'].pop().split(';')[0];
-          done();
-        });
-    });
+  /* admin jogosultsággal futtatható fv-ek tesztelése, ahol
+     most nincs böngésző ami 'megjegyezze' a bejelentkezett admint,
+     így manuálisan kell a http headerből a cookie-t hozzácsapni a fv-hez
+  */
+  before((done) => {
+    chai.request('http://localhost:8080/user')
+      .post('/login')
+      .send(theAccount)
+      .end((err, res) => {
+        if (err) {
+          throw err;
+        }
+        cookie = res.headers['set-cookie'].pop().split(';')[0];
+        done();
+      });
+  });
 
+  describe('editUser()', () => {
     it('response statusCode equal to 200', (done) => {
       chai.request(baseUrl)
         .put('/5afab001e8a028273ccecb24')
