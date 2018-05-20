ea25f529f5fd0c62aba9890bcdd60ba07538e1fe
Doni Pataki <yellow.pataki.doni@gmail.com>
2018-05-20 17:31:23 +0200

    Feat: jelszómódosítás backend + jsdoc

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -1,4 +1,5 @@
 const User = require('../models/user');
+
 /** @module User */
 module.exports = {
   profile: (req, res) => {
@@ -46,5 +47,29 @@ module.exports = {
     res.json({
       success: 'Sikeres kilépés',
     });
+  },
+  /**
+   * Jelszómódosítás
+   * Megváltoztatja a felhasználó jelszavát.
+   * Feltétele, hogy a felhasználó be legyen jelentkezve, és helyesen adja meg a régi jelszót.
+   * @param {Object} req - HTTP request objektum
+   * @param {Object} res - HTTP response objektum
+   * @returns {Object} - sikeres jelszóváltoztatás esetén success tulajdonságú objektumot ad vissza, hiba esetén err tulajdonságú objektumot küld
+   */
+  changePass: (req, res) => {
+    if (req.user) {
+      if (req.user['_id'] == req.params.id) {
+        User.findById(req.params.id).then((user) => {
+          user.changePassword(req.body.password, req.body.newPassword, (passwordErr) => {
+            if (passwordErr) {
+              res.status(401).json({ err: 'Rossz jelszó' });
+            } else {
+              user.save();
+              res.status(200).json({ success: 'Jelszó sikeresen megváltozott' });
+            }
+          });
+        });
+      } else { res.status(403).json({ err: 'Tiltott művelet' }); }
+    } else { res.status(401).json({ err: 'Nincs bejelentkezve' }); }
   },
 };
