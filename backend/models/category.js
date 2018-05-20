e167a7b22957dcf9458978786dbe73b5ce574279
Mihaly Tamas <mihaly.tamas92@gmail.com>
2018-05-20 23:42:26 +0200

    feat:new category table

---
+++ backend/models/category.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const CategorySchema = mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+}, {
+  timestamps: true,
+});
+
+module.exports = mongoose.model('Category', CategorySchema);
