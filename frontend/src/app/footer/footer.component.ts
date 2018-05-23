2909ee00e472bf758a1af8fff8a66100ef91bc7a
Szücs Máté <none@ofyourbiz.com>
2018-05-23 17:50:32 +0200

    jav

--- frontend/src/app/footer/footer.component.ts
+++ frontend/src/app/footer/footer.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
-
+  d = new Date();
+  cY = (this.d.getFullYear());
   constructor() { }
 
   ngOnInit() {
   }
-
 }
